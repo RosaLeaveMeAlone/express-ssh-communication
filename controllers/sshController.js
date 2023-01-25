c9e192279c064efd18b5d6fca3e09bffd1bfcd6d
Joel Uzcategui <joel@tianguis-mx.com>
2023-01-24 21:24:03 -0400

    first steps with ssh2 package

--- controllers/sshController.js
+++ controllers/sshController.js
@@ -1,6 +1,38 @@
 const { response } = require('express');
+const { readFileSync } = require('fs');
+
+const { Client } = require('ssh2');
 
 const devices = async(req, res = response) => {
+
+    const conn = new Client();
+    console.log('Primero de todo');
+    console.log('Host:',process.env.SSH_HOST);
+    await conn.on('ready', () => {
+        console.log('Client :: ready');
+        conn.exec('timedatectl', (err, stream) => {
+            if (err) throw err;
+            stream.on('close', (code, signal) => {
+                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                conn.end();
+            }).on('data', (data) => {
+                console.log('STDOUT: ' + data);
+            }).stderr.on('data', (data) => {
+                console.log('STDERR: ' + data);
+            });
+        });
+    }).connect({
+        host: process.env.SSH_HOST,
+        port: process.env.SSH_PORT,
+        username: process.env.SSH_USER,
+        password: process.env.SSH_PASSWORD,
+    });
+    console.log('Antes del json');
+    // const ssh = new SSH({
+    //     host: '167.235.238.223',
+    //     user: 'root',
+    //     pass: 'BUKmS5'
+    // });
     res.json({
         'msg' : 'Devices'
     });
